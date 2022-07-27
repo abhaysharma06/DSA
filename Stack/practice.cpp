5803d7f9defa3574264d7218fee96951465234da
Abhay Sharma <74869287+codeforgrow@users.noreply.github.com>
2022-07-27 19:46:47 +0530

    ✨ : stact implementation using node allocation

---
+++ Stack/practice.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+  // Write C++ code here
+  // std::cout << "Hello world!";
+  int a[4] = {8, -8, 7, 9};
+
+  int count = 0;
+  for (int i = 0; i < 4; i + 2)
+  {
+    if ((a[i] + a[i + 1]) != 0)
+    {
+      count++;
+    }
+  }
+  if (count > 0)
+    cout << "No";
+  else
+    cout << "Yes";
+}
